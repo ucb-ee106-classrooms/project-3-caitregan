cf78e104c91f2fc1804d342d5b58e81540b79783
Caitlin Regan <caitlin.regan@berkeley.edu>
2025-03-08 19:14:41 -0800

    dead reckoning turtlebot intial trial

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/caitlin/project3/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/caitlin/project3/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/caitlin/project3/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/caitlin/project3/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/caitlin/project3/src:$ROS_PACKAGE_PATH"
---
+++ build/project3/turtlebot_proj3_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/caitlin/project3/src/project3/turtlebot_proj3_pkg
+# Build directory: /home/caitlin/project3/build/project3/turtlebot_proj3_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
